f573094e0b69ff5c2322da00599e6e499194846f
Magus <magus@bgforge.net>
2021-08-21 18:22:43 +0700

    removed config reading on each event

--- f2gm.py
+++ f2gm.py
@@ -133,7 +133,7 @@ while True:  # Event Loop
 
   event, values = window.read()
 
-  print("event = {}".format(event))
+  # print("event = {}".format(event))
   if event == sg.WIN_CLOSED:
     break
   if event == "Save":
@@ -147,7 +147,7 @@ while True:  # Event Loop
       game_list(set_to_index=new_game_index)
     else:
       sg.popup("fallout2.exe not found in directory {}".format(dname))
-  if values['-LIST-']:
+  if values['-LIST-'] and cpaths == []:
     game_path = values['-LIST-'][0]
     cpaths = config_paths()
   cpaths = handle_event(window, event, values, game_path, cpaths)
