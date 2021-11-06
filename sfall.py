ec88c4b182f7afbb10c459d8286667f5f5ffecda
Magus <magus@bgforge.net>
2021-11-06 11:51:57 +0700

    allow offline work

--- sfall.py
+++ sfall.py
@@ -6,35 +6,41 @@ import subprocess
 import os
 
 def file_list():
-  files = {}
-  found = False
-  resp = requests.get('https://sourceforge.net/projects/sfall/files/sfall/')
-  soup = BeautifulSoup(resp.content, "lxml")
-  for tag in soup.find_all('script'):
-    for tc in tag.contents:
-      if 'net.sf.files' in tc:
-        m = tc.split('net.sf.staging_days')[0].strip()
-        m = m.replace('net.sf.files = ', '').rstrip(';')
-        jfiles = json.loads(m)
-        found = True
+  try:
+    files = {}
+    found = False
+    resp = requests.get('https://sourceforge.net/projects/sfall/files/sfall/')
+    soup = BeautifulSoup(resp.content, "lxml")
+    for tag in soup.find_all('script'):
+      for tc in tag.contents:
+        if 'net.sf.files' in tc:
+          m = tc.split('net.sf.staging_days')[0].strip()
+          m = m.replace('net.sf.files = ', '').rstrip(';')
+          jfiles = json.loads(m)
+          found = True
+          break
+      if found:
         break
-    if found:
-      break
 
-  for jf in jfiles:
-    if 'sfall_' in jf:
-      files[jf] = jfiles[jf]['download_url']
-  print(files)
-  return files
+    for jf in jfiles:
+      if 'sfall_' in jf:
+        files[jf] = jfiles[jf]['download_url']
+    print(files)
+    return files
+  except:
+    return {}
 
 def get_latest():
-  resp = requests.get('https://sourceforge.net/projects/sfall/best_release.json')
-  release = resp.json()['release']
-  latest = {}
-  latest['ver'] = release['filename'].split('_')[-1].rsplit('.',1)[0]
-  latest['url'] = release['url']
-  file_list()
-  return latest
+  try:
+    resp = requests.get('https://sourceforge.net/projects/sfall/best_release.json')
+    release = resp.json()['release']
+    latest = {}
+    latest['ver'] = release['filename'].split('_')[-1].rsplit('.',1)[0]
+    latest['url'] = release['url']
+    file_list()
+    return latest
+  except:
+    return {'ver': 'unknown', 'url': ''}
 
 def get_current(path):
   proc1 = subprocess.Popen(['strings', os.path.join(path, 'ddraw.dll')], stdout=subprocess.PIPE)
