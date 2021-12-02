7fe4cbde53b518cdecce961a1e01ecacafc0d156
Magus <magus@bgforge.net>
2021-12-03 04:50:37 +0700

    add ruamel sxplicitly

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ import sys
 sys.argv.append("py2exe")
 
 setup(
-    options={"py2exe": {"bundle_files": 3, "compressed": True}},
+    options={"py2exe": {"bundle_files": 3, "compressed": True, "includes": ["ruamel.yaml"]}},
     windows=[{"script": "zax.py"}],
 )
