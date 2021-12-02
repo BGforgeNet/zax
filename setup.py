bf90e725f1d6119d40bd5de75383148a6651bb2c
Magus <magus@bgforge.net>
2021-12-03 06:56:24 +0700

    add qt platform

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from distutils.core import setup
 import py2exe
 import sys
 import os
+import site
 
 sys.argv.append("py2exe")
 
@@ -11,6 +12,9 @@ for f in os.listdir("formats"):
     if os.path.isfile(rel_path):  # skip directories
         f2 = "formats", [rel_path]
         data_files.append(f2)
+platforms_dir = os.path.join(site.getsitepackages()[1], "PySide2", "Qt", "plugins", "platforms")
+qwindows_dll = os.path.join(platforms_dir, "qwindows.dll")
+data_files.append("platforms", [qwindows_dll])
 
 setup(
     options={"py2exe": {"bundle_files": 3, "compressed": True}},
