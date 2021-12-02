35ab612c24c4345e83de274a7ed6d237bda1ea3e
Magus <magus@bgforge.net>
2021-12-03 05:26:09 +0700

    fix data path

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ import os
 sys.argv.append("py2exe")
 
 data_files = []
-for files in os.listdir("formats"):
-    f1 = "formats" + files
-    if os.path.isfile(f1):  # skip directories
-        f2 = "formats", [f1]
+for f in os.listdir("formats"):
+    rel_path = os.path.join("formats", f)
+    if os.path.isfile(rel_path):  # skip directories
+        f2 = "formats", [rel_path]
         data_files.append(f2)
 
 setup(
