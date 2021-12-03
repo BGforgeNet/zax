0c36005f2107062df3b1655f1b6794b87be7546b
Magus <magus@bgforge.net>
2021-12-03 07:00:28 +0700

    type: tuple

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ for f in os.listdir("formats"):
         data_files.append(f2)
 platforms_dir = os.path.join(site.getsitepackages()[1], "PySide2", "Qt", "plugins", "platforms")
 qwindows_dll = os.path.join(platforms_dir, "qwindows.dll")
-data_files.append("platforms", [qwindows_dll])
+data_files.append(("platforms", [qwindows_dll]))
 
 setup(
     options={"py2exe": {"bundle_files": 3, "compressed": True}},
