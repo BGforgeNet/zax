f1fb3a90dd309398a5d47be150a4d9deb6d0a6be
Magus <magus@bgforge.net>
2021-12-03 07:24:40 +0700

    add plugin

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ for f in os.listdir("formats"):
     if os.path.isfile(rel_path):  # skip directories
         f2 = "formats", [rel_path]
         data_files.append(f2)
-platforms_dir = os.path.join(site.getsitepackages()[1], "PySide2", "Qt", "plugins", "platforms")
+platforms_dir = os.path.join(site.getsitepackages()[1], "PySide2", "plugins", "platforms")
 qwindows_dll = os.path.join(platforms_dir, "qwindows.dll")
 data_files.append(("platforms", [qwindows_dll]))
 
